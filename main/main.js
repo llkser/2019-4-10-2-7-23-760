9c0ba8e71664f477df8ca52c49762bf795e4bc16
Kang Liu <sc17kl@leeds.ac.uk>
2019-05-22 16:15:11 +0800

    Finished

--- main/main.js
+++ main/main.js
@@ -1,4 +1,4 @@
 module.exports = function main(inputs) {
     // write your code here...
-    return "price";
+    return Math.round(inputs.distance<=2?6+0.25*inputs.parkTime:6+0.8*(inputs.distance-2)+0.25*inputs.parkTime+0.4*Math.max(0,inputs.distance-8));
 };
